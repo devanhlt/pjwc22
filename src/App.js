a6e3cdb45624a7eb8b7a76ff75a11827f82e63d9
Anh Le <dev.anhlt@gmail.com>
2022-11-21 14:45:21 +0700

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDYBGg_5A4Mh29Mb-46w7YHkneXwCJtMuA",
+    authDomain: "pjwc22.firebaseapp.com",
+    projectId: "pjwc22",
+    storageBucket: "pjwc22.appspot.com",
+    messagingSenderId: "686750154057",
+    appId: "1:686750154057:web:04f072cab2430fbad2ee98",
+    measurementId: "G-CXD86LP72E",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+  // Get a list of cities from your database
+  async function getCities() {
+    const citiesCol = collection(db, "cities");
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map((doc) => doc.data());
+    return cityList;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +43,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>{JSON.stringify(getCities())}</div>
       </header>
     </div>
   );
